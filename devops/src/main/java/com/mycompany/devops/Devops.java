8959ff4da3f3a7462d2131542a625a97e771debc
Subikshaas12 <subiksha.502261@sxcce.edu.in>
2025-05-09 19:05:39 +0530

    Add files via upload

---
+++ devops/src/main/java/com/mycompany/devops/Devops.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.devops;
+
+/**
+ *
+ * @author subik
+ */
+public class Devops {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
